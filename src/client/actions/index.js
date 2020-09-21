5eeb7bfa29b26102b034d850f23f39e5a9947779
Dhruv Pathak <dhruvpathak9305@gmail.com>
2020-09-21 20:23:26 +0530

    Actions, DataUtil added

---
+++ src/client/actions/index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+import dataUtils from "../utils/dataUtils";
+
+export const FETCH_MISSIONS = "fetch_articles";
+
+export const fetchMissions = (searchString = "") => async (dispatch) => {
+  let url = `https://api.spaceXdata.com/v3/launches?limit=100&${searchString.slice(
+    1
+  )}`;
+
+  const res = await axios.get(url);
+
+  const formatData = dataUtils.formatLaunchData(res.data);
+  // console.log(formatData);
+
+  dispatch({
+    type: FETCH_MISSIONS,
+    payload: formatData,
+  });
+};
