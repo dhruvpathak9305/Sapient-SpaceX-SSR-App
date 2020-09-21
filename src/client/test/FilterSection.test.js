8f6f7e4641020bb03a6a98f3d23930baea612965
Dhruv Pathak <dhruvpathak9305@gmail.com>
2020-09-21 20:24:43 +0530

    Added Jest Test Cases

---
+++ src/client/test/FilterSection.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow } from "enzyme";
+import FilterGroup from "../../../../SpaceX-1-SSRApp/spacexReactSSR/src/client/components/FilterGroup";
+
+describe("MyComponent", () => {
+  it("should render correctly", () => {
+    const component = shallow(<FilterGroup />);
+  });
+});
