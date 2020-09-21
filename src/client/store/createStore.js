1a341fa861be343d3965e19e01cfa6a04c2eb908
Dhruv Pathak <dhruvpathak9305@gmail.com>
2020-09-21 20:19:01 +0530

    Redux Store and reducer refactoring done

---
+++ src/client/store/createStore.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from '../../../../SpaceX-1-SSRApp/spacexReactSSR/src/client/reducers';
+
+export default () => {
+  const store = createStore(reducers, {}, applyMiddleware(thunk));
+  return store;
+};
